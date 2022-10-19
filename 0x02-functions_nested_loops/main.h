32247126e136f0ff1e7a6f2b2637c1a08cf36624
Makebesylvie <makebesylvie@yahoo.com>
2022-10-18 20:12:21 -0700

    k

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,37 +1,5 @@
-#include "main.h"
+#ifdef MAIN_H
+#define MAIN_H
+int _putchar(char c)
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{
-
-		char *sh = "main.h";
-
-
-
-			while (*sh)
-
-					{
-
-								_putchar(*sh);
-
-										sh++;
-
-											}
-
-				_putchar('\n');
-
-
-
-					return (0);
-
-}
+#endif
